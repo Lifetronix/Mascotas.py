67e18a1ca6d9047a4d00effe506b48fb01d9873b
Julio Moreno <julio.morenoshz@uanl.edu.mx>
2022-01-06 01:01:35 -0600

    modified index oviparos

--- index.py
+++ index.py
@@ -1,4 +1,10 @@
-from Mamiferos import  Mammal
+from Mamiferos.Mamifero import  Mammal
+from Oviparos.oviparos import Oviparous
+
 perro=Mammal(4)
 perro.isborning()
 print(perro.numlegsw)
+
+pez = Oviparous(0)
+pez.isborning()
+print(pez.numlegsw)
